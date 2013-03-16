2ef3eabfc2e27ed393006981e6405f1f07806a12
Gideon Farrell <gideon@Gidbook.local>
2013-03-16 14:01:40 +0000

    Added main class config

---
+++ config/auth.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    // Which colleges to allow (King's Undergraduates)
+    'allow_colleges'    =>  array('KINGSUG'),
+
+    // What model class to use
+    'model'             =>  '\Ravenly\Models\RavenUser'
+);
+?>
