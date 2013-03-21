b9db4a1682455dd7e83e57768b3dfbe3bec7a155
Gideon Farrell <gideon@Gidbook.local>
2013-03-21 23:17:45 +0000

    Added some authentication conditions to the config

--- config/auth.php
+++ config/auth.php
@@ -1,9 +1,24 @@
 <?php
 return array(
-    // Which colleges to allow (King's Undergraduates)
-    'allow_colleges'    =>  array('KINGSUG'),
+    // Authentication conditions
+    'conditions'        =>  array(
+        // Does a user have to belong to a list of crsids (default: no)
+        'crsid'             =>  false,
+        
+        // Which colleges to allow (e.g. King's Undergraduates)
+        'collegecode'       =>  array('KINGSUG'),
+
+        // Does a user have to exist in the database (default: no)
+        'force_db'          =>  false,
+
+        // Does the user have to be in a particular user group (default: no)
+        'group'             =>  false,
+    ),
 
     // What model class to use
-    'model'             =>  '\Ravenly\Models\RavenUser'
+    'model'             =>  '\Ravenly\Models\RavenUser',
+
+    // Automatically create a new User if one isn't found?
+    'autocreate'        =>  false
 );
 ?>
