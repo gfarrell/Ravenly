42d6ffe2349c6710d7972f8cddafbef9340d2b32
Gideon Farrell <gideon@Gidbook.local>
2013-03-21 23:18:16 +0000

    Add some more instance methods with docs

--- models/raven_user.php
+++ models/raven_user.php
@@ -2,7 +2,34 @@
 namespace Ravenly\Models;
 
 class RavenUser extends Eloquent{
-    protected function fillFromLookup() {
+    /* --- Instance Methods --- */
+
+    /**
+     * Returns user's groups
+     * @return Relationship the users groups
+     */
+    public function groups() {
+        return $this->has_many_and_belongs_to('Group');
+    }
+
+    /**
+     * Checks if a user is in a group or groups
+     * @param  string|array $groups group name or an array of possible group names
+     * @return boolean
+     */
+    public function inGroup($group) {
+        if(!is_array($group)) $group = array($group);
+
+        $gg = Set::extract('name', $this->groups()->get());
+
+        return (count(array_intersect($gg, $group)) > 0);
+    }
+
+    /**
+     * Fills fields from LDAP lookup
+     * @return void
+     */
+    public function fillFromLookup() {
         $lookup = RavenUser::lookup($crsid);
         if(is_array($lookup)) {
             foreach($lookup as $field => $value) {
