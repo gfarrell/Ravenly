b580dadd2fe525e31eb5a88017c76ef55dd8a562
Gideon Farrell <gideon@Gidbook.local>
2013-03-22 11:52:01 +0000

    Logging!

--- routes.php
+++ routes.php
@@ -1,12 +1,16 @@
 <?php
 Route::filter('raven', function($conditions = array()) {
     if(!Ravenly\Ravenly::loggedIn()) {
+    Log::info('Ravenly: raven filter initiated.');
+        Log::info('Ravenly: - user already logged in, authenticating.');
         $status = Ravenly\Ravenly::authenticate(Ravenly\Ravenly::getUser(), $conditions);
     } else {
+        Log::info('Ravenly: - user not logged in, logging in.');
         $status = Ravenly\Ravenly::login();
     }
 
     if(!$status) {
+        Log::info('Ravenly: [!] not authorised.');
         return Response::error(403);
     }
 });
