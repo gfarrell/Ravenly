2955e16575c5dac1c0083601c69a7cebcc19fb62
Gideon Farrell <gideon@Gidbook.local>
2013-03-26 16:52:34 +0000

    Got rid of all the start.php configuration and added a logging line.

--- start.php
+++ start.php
@@ -1,8 +1,3 @@
 <?php
-
-Autoloader::namespaces(array(
-    'Ravenly'   =>  Bundle::path('ravenly').'ravenly.php',
-    'RavenUser' =>  Bundle::path('ravenly').'models/raven_user.php'
-));
-
+Log::info('Ravenly online');
 ?>
