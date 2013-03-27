d15cd0d727358b018aa15c14a58513bfc97ccb0d
Gideon Farrell <gideon@Gidbook.local>
2013-03-27 01:36:17 +0000

    Added user group model

---
+++ models/usergroup.php
@@ -0,0 +1,14 @@
+<?php
+namespace Ravenly\Models;
+
+use Eloquent;
+
+class UserGroup extends Eloquent {
+    public static $table = 'user_groups';
+
+    public function users()
+    {
+        return $this->has_many_and_belongs_to('RavenUser');
+    }
+}
+?>
